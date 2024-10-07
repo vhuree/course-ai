61d96c7772bc687d138d84661be9bcb20a2c2d42
Vikas Huree <vikas.huree@gmail.com>
2024-10-07 09:05:56 -0700

    Initial commit

---
+++ src/main/java/com/elearning/ai/CourseAiApplication.java
@@ -0,0 +1,13 @@
+package com.elearning.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elearning/ai/CourseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearning.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
